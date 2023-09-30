2ad2a0c0e1338c4e36f32772960de337e85e25b6
Pablo Guerra <pguerracamana@gmail.com>
2023-09-29 23:10:24 -0500

    Mas comentarios a clases

--- src/main/java/com/api/aerolinea/Config/SecurityConfig.java
+++ src/main/java/com/api/aerolinea/Config/SecurityConfig.java
@@ -11,32 +11,47 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration
-@EnableWebSecurity
+@Configuration // Indica que esta clase es una configuración de Spring.
+@EnableWebSecurity // Habilita la configuración de seguridad web de Spring.
 public class SecurityConfig  {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final AuthenticationProvider authenticationProvider;
 
+    /**
+     * Constructor de SecurityConfig que inyecta JwtAuthenticationFilter y AuthenticationProvider.
+     *
+     * @param jwtAuthenticationFilter El filtro de autenticación JWT personalizado.
+     * @param authenticationProvider El proveedor de autenticación personalizado.
+     */
     public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, AuthenticationProvider authenticationProvider) {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
         this.authenticationProvider = authenticationProvider;
     }
 
+    /**
+     * Configura la cadena de filtros de seguridad de Spring.
+     *
+     * @param http El objeto HttpSecurity que se configura para establecer políticas de seguridad.
+     * @return La cadena de filtros de seguridad configurada.
+     * @throws Exception Si hay excepciones al configurar la seguridad.
+     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable) // Deshabilita la protección CSRF.
                 .authorizeHttpRequests(authRequest ->
                         authRequest
-                                .requestMatchers("/**").permitAll()
-                                .anyRequest().authenticated()
+                                .requestMatchers("/**").permitAll() // Permite todas las solicitudes para cualquier URL.
+                                .anyRequest().authenticated() // Requiere autenticación para todas las demás solicitudes.
                         )
                 .sessionManagement(sessionManager ->
                         sessionManager
-                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Configura la administración de
+                                                                                        // sesiones como sin estado.
                         )
-                .authenticationProvider(authenticationProvider)
+                .authenticationProvider(authenticationProvider) // Establece el proveedor de autenticación.
+                // Agrega un filtro personalizado antes del filtro de autenticación por nombre de usuario y contraseña.
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
+                .build(); // Construye y devuelve la cadena de filtros de seguridad.
     }
 }
